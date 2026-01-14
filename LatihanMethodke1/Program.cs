cf031743866777ab7c6996d70c191747954d2768
yoga759 <nlputraa058@gmail.com>
2026-01-14 14:23:16 +0700

    Update Program.cs
    Membuat method tambah,kurang,kali dan bagi

--- LatihanMethodke1/Program.cs
+++ LatihanMethodke1/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 10 , b = 2, c = 0;
+
+ void  Tambah()
+ {
+
+    c = a + b;
+    Console.WriteLine($"{a} + {b} = {c}");
+    
+ }
+
+void kurang()
+{
+
+    c = a - b;
+    Console.WriteLine($"{a} - {b} = {c}");
+
+}
+
+void kali()
+{
+
+    c = a + b;
+    Console.WriteLine($"{a} x {b} = {c}");
+
+}
+
+void Bagi()
+{
+
+    c = a + b;
+    Console.WriteLine($"{a} / a{b} = {c}");
+
+}
+
+
+
+
+
+
+
